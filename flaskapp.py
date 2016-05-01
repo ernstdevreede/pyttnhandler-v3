6b2cb82c75d7604e6dbe2893626cb8ac79ef2f93
Ernst de Vreede <ernstdevreede@xs4all.nl>
2016-05-01 14:33:20 +0200

    First pymongo test

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,24 @@
+import sys
+import os
+
 from flask import Flask
 app = Flask(__name__)
+from pymongo import MongoClient
+
+mongo=None
+db=None
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+  global mongo
+  global db
+  if not db:
+    mongo=MongoClient('ds011412.mlab.com',11412)
+    mongo.lora.authenticate("lora_ingester", "lora")
+    db=mongo["lora"]
+
+  cnt=db.uplinks.find().count()
+  return 'Hello World!'+" %d messages"%cnt
 
 if __name__ == '__main__':
     app.run()
